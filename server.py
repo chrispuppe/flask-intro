a464f9e9ab612805d2007f937a29b2a6e302ad8b
Chris Puppe <chrispuppe@gmail.com>
2022-04-16 12:16:27 -0700

    Assignment complete

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 from random import choice
 
-from flask import Flask, request
+from flask import Flask, request, url_for, render_template
 
 # "__name__" is a special Python variable for the name of the current module
 # Flask wants to know this to know what any imported things are relative to.
@@ -13,33 +13,28 @@ AWESOMENESS = [
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
     'wonderful', 'smashing', 'lovely']
 
+WEAKNESS = [
+    'lame', 'dumb', 'weak', 'slimy', 'smelley', 'meh',
+    'mediocre', 'homely', 'messy', 'dirty']
+
 
 @app.route('/')
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+      <!doctype html>
+        <html>
+          Hi! This is the home page.
+          <a href="/hello">Welcome Page</a>
+        </html>"""
 
 
 @app.route('/hello')
 def say_hello():
     """Say hello and prompt for user's name."""
-
-    return """
-    <!doctype html>
-    <html>
-      <head>
-        <title>Hi There!</title>
-      </head>
-      <body>
-        <h1>Hi There!</h1>
-        <form action="/greet">
-          What's your name? <input type="text" name="person">
-          <input type="submit" value="Submit">
-        </form>
-      </body>
-    </html>
-    """
+    
+    return render_template('hello.html', AWESOMENESS=AWESOMENESS, WEAKNESS=WEAKNESS)
 
 
 @app.route('/greet')
@@ -48,7 +43,7 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliment = request.args.get('compliment')
 
     return f"""
     <!doctype html>
@@ -58,6 +53,26 @@ def greet_person():
       </head>
       <body>
         Hi, {player}! I think you're {compliment}!
+      </body>
+    </html>
+    """
+
+@app.route('/diss')
+def diss_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    insult = request.args.get('insult')
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {insult}!
       </body>
     </html>
     """
